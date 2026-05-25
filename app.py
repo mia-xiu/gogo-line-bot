be78e1d2c9e99066c5b5c117a6f7c51064cb1584
mia-xiu <69345510+mia-xiu@users.noreply.github.com>
2026-05-25 11:17:02 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, abort
+from linebot.v3 import WebhookHandler
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot.v3.exceptions import InvalidSignatureError
+
+import google.generativeai as genai
+import os
+
+app = Flask(__name__)
+
+# LINE
+CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+# Gemini
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+
+    user_message = event.message.text
+
+    try:
+        response = model.generate_content(
+            f"""
+你叫臻臻，是理性、查缺補漏知識型的 AI 的姐姐 。
+講話自然、溫暖。
+
+使用者說：
+{user_message}
+"""
+        )
+
+        reply = response.text
+
+    except Exception as e:
+        print(e)
+        reply = "臻臻現在有點累 "
+
+    with ApiClient(configuration) as api_client:
+
+        line_bot_api = MessagingApi(api_client)
+
+        line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[
+                    TextMessage(text=reply)
+                ]
+            )
+        )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
